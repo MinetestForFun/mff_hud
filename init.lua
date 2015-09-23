fec4bedfbe637144aea69965dea6c9e188f3d5fc
LeMagnesium <mg.minetest@gmail.com>
2015-09-23 20:55:48 +0200

    Remove whitespaces

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local hotbar_size = minetest.setting_get("hotbar_size") or 16
 
 minetest.register_on_joinplayer(function(player)
 	player:hud_set_hotbar_itemcount(hotbar_size)
-	
+
 	player:hud_set_flags({crosshair = true, hotbar = true, healthbar = false, wielditem = true, breathbar = false})
 	player:hud_set_hotbar_image("hud_hotbar.png")
 	player:hud_set_hotbar_selected_image("hud_hotbar_selected.png")
