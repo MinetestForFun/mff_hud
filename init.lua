bbff05d22fab878e6b60ba8ec738dd955ebd1eaa
crabman77 <crabman77.github@gmail.com>
2015-10-22 22:29:50 +0200

    set minimap

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
 local hotbar_size = minetest.setting_get("hotbar_size") or 16
 local health_visible = minetest.setting_getbool("hotbar_health_visible") or false
+local minimap_visible = minetest.setting_getbool("enable_minimap") or false
 
 minetest.register_on_joinplayer(function(player)
 	player:hud_set_hotbar_itemcount(hotbar_size)
 
-	player:hud_set_flags({crosshair = true, hotbar = true, healthbar = health_visible, wielditem = true, breathbar = true})
+	player:hud_set_flags({crosshair = true, hotbar = true, healthbar = health_visible, wielditem = true, breathbar = true, minimap = minimap_visible})
 	player:hud_set_hotbar_image("hud_hotbar.png")
 	player:hud_set_hotbar_selected_image("hud_hotbar_selected.png")
 end)
