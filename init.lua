f8dfc851ec5c45c4799e53fe8ede127d3236bc17
LeMagnesium <mg.minetest@gmail.com>
2015-10-26 23:03:38 +0100

    Enable breathbar only when healthbar is visible

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ local minimap_visible = minetest.setting_getbool("enable_minimap") or false
 minetest.register_on_joinplayer(function(player)
 	player:hud_set_hotbar_itemcount(hotbar_size)
 
-	player:hud_set_flags({crosshair = true, hotbar = true, healthbar = health_visible, wielditem = true, breathbar = true, minimap = minimap_visible})
+	player:hud_set_flags({crosshair = true, hotbar = true, healthbar = health_visible, wielditem = true, breathbar = health_visible, minimap = minimap_visible})
 	player:hud_set_hotbar_image("hud_hotbar.png")
 	player:hud_set_hotbar_selected_image("hud_hotbar_selected.png")
 end)
